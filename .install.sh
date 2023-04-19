e2a0f2e6ecd175dd95fd9eb329a813e41ea3c690
eyerust <32106337+eyerust@users.noreply.github.com>
2023-04-19 16:32:40 +0200

    Add install script

---
+++ .install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
