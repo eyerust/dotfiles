80c94a2319308ea4407bd597064c21381b6ef289
eyerust <32106337+eyerust@users.noreply.github.com>
2023-04-19 16:36:12 +0200

    Modify install script

--- .install.sh
+++ .install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cp -a ./.zshrc $HOME/.zshrc
+cp -a ./.p10k.zsh $HOME/.p10k.zsh
+cp -a ./.oh-my-zsh $HOME/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
